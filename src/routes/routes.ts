ece62dfeb538308987116efd037daab6cc29b903
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-12-30 12:48:50 -0300

    chore: rename admin route to routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import * as auth from "../controllers/auth";
+import * as members from "../controllers/members";
+import * as users from "../controllers/users";
+
+const router = Router();
+
+router.post("/login", auth.login);
+
+router.get("/ping", (req, res) => {
+    res.json({ pong: true, admin: true });
+});
+
+router.post("/users", users.createUser);
+
+router.get("/members", members.getAll);
+router.get("/members/search", members.search);
+router.get("/members/:id", members.getOne);
+router.post("/members", members.createMember);
+router.put("/members/:id", members.updateMember);
+router.delete("/members/:id", members.removeMember);
+
+export default router;
